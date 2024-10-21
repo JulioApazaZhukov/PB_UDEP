3444bb57fbc84b44a496c23127dc285af30ba986
Julio Apaza <79545504+JulioApazaZhukov@users.noreply.github.com>
2024-10-21 06:54:18 -0500

    10-21-2024

--- Ejercicios_resueltos/13.1.py
+++ Ejercicios_resueltos/13.1.py
@@ -1,5 +1,5 @@
 while True:
-    año = int(input('Ingrese el año:'))
+    año = int(input('Ingrese el año: '))
     if año>=1900 and año<2100:
         break
 
@@ -7,12 +7,13 @@ bisiesto=0
 if año%4==0 and (año%100!=0 or año%400==0):
     bisiesto=1
 while True:
-    mes = int(input('Ingrese el mes:'))
+    mes = int(input('Ingrese el mes: '))
     if mes>=1 and mes<=12:
         break
 
 diasMes = [31,28+bisiesto,31,30,31,30,31,31,30,31,30,31]
 while True:
-    dia = int(input('Ingrese el día:'))
+    dia = int(input('Ingrese el día: '))
     if dia>=1 and dia<= diasMes[mes-1]:
+        print(f"Fecha válida: {dia}/{mes}/{año}")
         break
