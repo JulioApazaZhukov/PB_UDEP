b3fe3713d63fe2d6213d05b58cc0a94e3cfabc30
Julio Apaza <julio7414.trading@gmail.com>
2024-11-05 08:48:23 -0500

    11-05-2024

---
+++ Implementaciones_extracurriculares/PC4_DSA.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdlib.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+// To do
+/*
+    Create dynamic matrix
+
+    Input:
+    11 5 8 7 9
+    9 52 4 2 5
+    2 3 40 7 2
+    9 5 1 49 4
+    8 7 5 2  11
+    Expected output
+*/
+
+/*
+
+void printMatrix(int);
+
+void printMatrix(int matrix);
+{
+    int matrixsize = sizeof(matrix) / sizeof(matrix[0][0]);
+    for (int i = 0; i < matrixsize; i++){
+		for (int j = 0; j < matrixsize; j++) {
+			if (matrix[i][j] < 10) {
+				cout << "0" << matrix[i][j] << " ";
+			} else { cout << matrix[i][j] << " "; }
+		}
+		cout << endl;
+	}
+}
+
+int main ()
+{
+    int matrix[][] = {{1, 5, 7}, {7, 6, 4}, {9, 4, 2}};
+    /*
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++){
+			cout << "Enter: " << i << " " << j << ":	";
+			std::cin >> matrix[i][j];
+		}
+	}
+    
+    printMatrix(matrix);
+}*/
